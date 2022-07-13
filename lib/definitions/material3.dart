a053d53b75758bf64e52ab52db5f93fde2fe4ad8
Pliavi <pliavi.rpgm@gmail.com>
2022-07-13 17:00:29 -0300

    separate the breakpoint definitions

---
+++ lib/definitions/material3.dart
@@ -0,0 +1,36 @@
+import 'package:breakpoint3/breakpoint3.dart';
+
+class Material3BreakpointDefinition {
+  static Map<String, BreakpointDefinition> breakpoints = {
+    'extraSmall': BreakpointDefinition(
+      maxWidth: 0,
+      columns: 1,
+      margin: 0,
+      gutter: 0,
+    ),
+    'small': BreakpointDefinition(
+      maxWidth: 0,
+      columns: 2,
+      margin: 0,
+      gutter: 0,
+    ),
+    'medium1': BreakpointDefinition(
+      maxWidth: 0,
+      columns: 3,
+      margin: 0,
+      gutter: 0,
+    ),
+    'medium2': BreakpointDefinition(
+      maxWidth: 0,
+      columns: 4,
+      margin: 0,
+      gutter: 0,
+    ),
+    'large': BreakpointDefinition(
+      maxWidth: 0,
+      columns: 5,
+      margin: 0,
+      gutter: 0,
+    ),
+  };
+}
